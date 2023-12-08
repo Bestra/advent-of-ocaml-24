5f1458f85f5d173ede7e1b0eb2621a94ff7fd339
Chris Westra <bestra@github.com>
2023-12-08 09:44:26 -0500

    initial parsing day 3

--- lib/day3.ml
+++ lib/day3.ml
@@ -7,29 +7,55 @@ module Parser = struct
   let is_digit = function '0' .. '9' -> true | _ -> false
 
   type t =
-    { row_number: int
-    ; column_number: int
+    { column_number: int
+    ; int_buffer: string
     ; entries: entry list
-    ; int_buffer: string }
+    ; row_number: int }
+
+  let flush_buffer t =
+    if String.is_empty t.int_buffer then t
+    else
+      let number = int_of_string t.int_buffer in
+      let entry = Number (number, t.row_number, t.column_number) in
+      { t with
+        int_buffer= ""
+      ; entries= entry :: t.entries
+      ; column_number= t.column_number + 1 }
+
+  let append_buffer t c = {t with int_buffer= t.int_buffer ^ String.make 1 c}
+
+  let symbol c t =
+    let entry = Symbol (c, t.row_number, t.column_number) in
+    {t with entries= entry :: t.entries}
+
+  let advance t = {t with column_number= t.column_number + 1}
+
+  let ingest_char c t =
+    match c with
+    | c when is_digit c ->
+        append_buffer t c
+    | '.' ->
+        flush_buffer t |> advance
+    | c ->
+        flush_buffer t |> symbol c |> advance
 
   let read_row row row_number =
-    let rec read_row' acc column int_buffer = function
+    let state = {column_number= 0; int_buffer= ""; entries= []; row_number} in
+    let row = String.to_list row in
+    let rec read_row' t l =
+      match l with
       | [] ->
-          acc
+          let accum = flush_buffer t in
+          List.rev accum.entries
       | c :: cs ->
-          if is_digit c then
-            read_row' acc (column + 1) (int_buffer ^ String.make 1 c) cs
-          else if String.is_empty int_buffer then
-            if Char.(c = '.') then read_row' acc (column + 1) "" cs
-            else
-              read_row'
-                (Symbol (c, row_number, column) :: acc)
-                (column + 1) "" cs
-          else
-            let number = int_of_string int_buffer in
-            read_row'
-              (Number (number, row_number, column) :: acc)
-              (column + 1) "" cs
+          let new_state = ingest_char c t in
+          read_row' new_state cs
     in
-    read_row' [] 0 "" row
+    read_row' state row
+
+  let%expect_test _ =
+    read_row "43.2.5" 0 |> [%sexp_of: entry list] |> print_s ;
+    [%expect {|
+        ((Number (43 0 0)) (Number (2 0 2))
+        |}]
 end
