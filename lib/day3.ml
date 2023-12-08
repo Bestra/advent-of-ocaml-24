71f39c985d98de32acc73251b860b3de8d8fb3fb
Chris Westra <bestra@github.com>
2023-12-08 10:46:55 -0500

    TODO

--- lib/day3.ml
+++ lib/day3.ml
@@ -1,5 +1,6 @@
 open Core
 
+(* TODO: change number to use a list of coordinates*)
 type entry = Symbol of (char * int * int) | Number of (int * int * int)
 [@@deriving sexp_of]
 
@@ -55,7 +56,53 @@ module Parser = struct
 
   let%expect_test _ =
     read_row "43.2.5" 0 |> [%sexp_of: entry list] |> print_s ;
+    [%expect
+      {|
+        ((Number (43 0 0)) (Number (2 0 2)) (Number (5 0 4)))
+        |}]
+
+  let%expect_test _ =
+    read_row "467..114.." 0 |> [%sexp_of: entry list] |> print_s ;
     [%expect {|
-        ((Number (43 0 0)) (Number (2 0 2))
+        ((Number (467 0 0)) (Number (114 0 3)))
+        |}]
+
+  let%expect_test _ =
+    read_row ".....+.58." 0 |> [%sexp_of: entry list] |> print_s ;
+    [%expect {|
+        ((Symbol (+ 0 5)) (Number (58 0 7)))
+        |}]
+
+  let sample_input =
+    [ "467..114.."
+    ; "...*......"
+    ; "..35..633."
+    ; "......#..."
+    ; "617*......"
+    ; ".....+.58."
+    ; "..592....."
+    ; "......755."
+    ; "...$.*...."
+    ; ".664.598.." ]
+
+  let parse_schematic schematic =
+    let rec parse_schematic' schematic row_number =
+      match schematic with
+      | [] ->
+          []
+      | row :: rows ->
+          let entries = read_row row row_number in
+          entries @ parse_schematic' rows (row_number + 1)
+    in
+    parse_schematic' schematic 0
+
+  let%expect_test _ =
+    parse_schematic sample_input |> [%sexp_of: entry list] |> print_s ;
+    [%expect
+      {|
+        ((Number (467 0 0)) (Number (114 0 3)) (Symbol (* 1 3)) (Number (35 2 2))
+         (Number (633 2 5)) (Symbol (# 3 6)) (Number (617 4 0)) (Symbol (* 4 1))
+         (Symbol (+ 5 5)) (Number (58 5 7)) (Number (592 6 2)) (Number (755 7 6))
+         (Symbol ($ 8 3)) (Symbol (* 8 5)) (Number (664 9 1)) (Number (598 9 3)))
         |}]
 end
